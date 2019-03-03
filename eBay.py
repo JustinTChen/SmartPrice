1804bb056ddf3c1014404f1799b9ea3c3d83859f
Justin Chen <jchen23@berkeley.edu>
2019-03-02 21:18:24 -0800

    Fixed some stuff

--- eBay.py
+++ eBay.py
@@ -9,24 +9,43 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+file_metadata = {'name': 'photo.jpg'}
+media = MediaFileUpload('files/photo.jpg',
+                        mimetype='image/jpeg')
+file = drive_service.files().create(body=file_metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+print 'File ID: %s' % file.get('id')
+
 #get_ipython().run_line_magic('matplotlib', 'inline')
 plt.style.use('fivethirtyeight')
 sns.set()
 sns.set_context("talk")
 
+##############################################################################
+"*** RETURNS CATEGORY ID FROM USER CATEGORY INPUT ***"
+##############################################################################
+def findID(category):
+    cats = Table().read_table("categories.csv")
+    return cats.where('Category', category).column('#').item(0)
+
 def calculate(search_term, category, condition):
+    ##########################################################################
+    "*** API ***"
+    ##########################################################################
     key = 'JustinCh-SmartPri-PRD-416e5579d-aa266db1'
+    category = findID(category)
     url = ("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=JustinCh-SmartPri-PRD-416e5579d-aa266db1&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + search_term + "&categoryId=" + str(category) + "&itemFilter(0).name=Condition&itemFilter(0).value=" + str(condition) + "&itemFilter(1).name=FreeShippingOnly&itemFilter(1).value=false&itemFilter(2).name=SoldItemsOnly&itemFilter(2).value=true")
 
+    ###########################################################################
+    "*** CLEANS UP .JSON TO PANDAS DATAFRAME ***"
+    ###########################################################################
     apiResult = requests.get(url)
     listings, prices, offer, BIN = [], [], [], []
-
     parsed = apiResult.json()
-
     numPages = int(parsed["findCompletedItemsResponse"][0]['paginationOutput'][0]['totalPages'][0])
     if numPages > 10:
         numPages = 10
-
     for page in range(1, numPages+1):
         apiResult_page = requests.get(url+"&paginationInput.pageNumber=" + str(page))
         parsed_page = apiResult_page.json()
@@ -35,7 +54,9 @@ def calculate(search_term, category, condition):
             prices.append(float(item['sellingStatus'][0]['convertedCurrentPrice'][0]['__value__']))
             offer.append(item['listingInfo'][0]['bestOfferEnabled'][0])
             BIN.append(item['listingInfo'][0]['buyItNowAvailable'][0])
-
+    ###########################################################################
+    "*** PANDAS OPERATIONS FROM TABLE TO GRAPH ***"
+    ###########################################################################
     dataTable = pd.concat([pd.Series(listings), pd.Series(prices), pd.Series(offer), pd.Series(BIN)], axis=1)
     dataTable.columns = ['Name', 'Price', 'Best Offer Enabled', "Buy It Now"]
     dataTable
@@ -50,5 +71,7 @@ def calculate(search_term, category, condition):
     plt.savefig('dist.png')
 
     summary = dataTable.describe()['Price']
-
+    ###########################################################################
+    "*** RETURN DICT OF IMPORTANT INFO ***"
+    ###########################################################################
     return {'Mean':summary[1], 'SD':summary[2], '25%':summary[4], '50%':summary[5], '75%':summary[6], 'Graph':1}
