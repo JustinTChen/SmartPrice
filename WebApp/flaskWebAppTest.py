34e9d4a9bcc220e03fef2ecfa378135c7238587d
Justin Chen <jchen23@berkeley.edu>
2019-03-02 23:51:40 -0800

    testing image

--- WebApp/flaskWebAppTest.py
+++ WebApp/flaskWebAppTest.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import render_template
+from eBay.py import *
+
+dict = calculate('iPhone+X+64+GB', 'Cell Phones & Accessories', 'New')
+graph = dict['Graph']
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('test.html')
+    return render_template('test.html', Graph=graph)
