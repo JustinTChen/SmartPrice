2e89faa85f7b0920827e9a45298d7a4df6ff6162
Mark Selden <seldenm@berkeley.edu>
2019-03-03 03:35:55 -0800

    final stages

--- WebApp/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class itemForm(FlaskForm):
-    item = StringField("Item:", validators=[DataRequired()])
-    button = SubmitField("Price Your Item!")
--- env.flaskenv
+++
@@ -1,2 +0,0 @@
-
-FLASK_APP=flaskWebAppTest.py
