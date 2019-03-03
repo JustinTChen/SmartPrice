189df81a531fed3c3a6798f35c84fdda70ea0e38
Mark Selden <seldenm@berkeley.edu>
2019-03-03 04:27:57 -0800

    completed all functionality, still need to style

--- WebApp/SmartPricer.py
+++ WebApp/SmartPricer.py
@@ -7,33 +7,29 @@ from eBay import *
 from datascience import *
 import numpy as np
 
- calculate('iPhone+X+64+GB', 'Cell Phones & Accessories', 'New')
-graph = dict['Graph']
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'flask-is-hard'
 
-class searchForm(Form):
+
+class searchForm(FlaskForm):
     cats = list(Table().read_table('categories.csv').column('Category'))
     cats_tuples = [(cat, cat) for cat in cats]
     item = StringField('Item', validators=[DataRequired()])
     categories = SelectField('Categories', choices=cats_tuples)
     condition = SelectField('Condition', choices=[('New', 'New'), ('Used', "Used")])
-    button = SubmitField('Price My Item!!')
+    submit = SubmitField('submit')
 
 
 
-@app.route("/price_it", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def Home():
     form = searchForm()
-
     if form.validate_on_submit():
         Item = form.item.data
         Cat = form.categories.data
         Cond = form.condition.data
         results = calculate(Item, Cat, Cond)
-
-        return render_template('/results')
+        return render_template('page2.html', image=results['Graph'] , mean=results['Mean'],SD=results['SD'],quarter=results['25%'], half=results['50%'], threequarters=results['75%'], min=results['Min'], max=results['Max'])
 
     return render_template('test.html', form = form)
-
-@
